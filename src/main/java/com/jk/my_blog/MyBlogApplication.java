9e394c69e57b4433b5e21fa160423e78ea9c7035
“Kawhi-95” <420021407@qq.com>
2023-08-25 19:31:21 +0800

    项目初始化成功,正常启动,健身去了!

--- src/main/java/com/jk/my_blog/MyBlogApplication.java
+++ src/main/java/com/jk/my_blog/MyBlogApplication.java
@@ -2,8 +2,10 @@ package com.jk.my_blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class })
 public class MyBlogApplication {
 
     public static void main(String[] args) {
