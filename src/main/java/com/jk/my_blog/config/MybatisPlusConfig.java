53b6f1fb1f89b3ff44c6d1ff1fe55d0b98858fd9
Akang <420021407@qq.com>
2023-08-26 22:09:14 +0800

    MybatisPlus的Config

---
+++ src/main/java/com/jk/my_blog/config/MybatisPlusConfig.java
@@ -0,0 +1,19 @@
+package com.jk.my_blog.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@MapperScan("com.jk.my_blog.mapper")
+public class MybatisPlusConfig {
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        return paginationInterceptor;
+    }
+}
