311cc3f82f048347c4cc5790936a0a2e95127cca
“Kawhi-95” <420021407@qq.com>
2023-08-27 17:21:29 +0800

    进行异常处理，并测试

--- src/main/java/com/jk/my_blog/controller/UserController.java
+++ src/main/java/com/jk/my_blog/controller/UserController.java
@@ -3,6 +3,7 @@ package com.jk.my_blog.controller;
 
 import com.jk.my_blog.common.lang.Result;
 import com.jk.my_blog.service.UserService;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,7 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @RequiresAuthentication
     @GetMapping("/test")
     public Result test(){
         return Result.success(userService.getById(1L));
