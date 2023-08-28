77e371d86a1668b9fb0c32117a8dc2b3369c372b
“Kawhi-95” <420021407@qq.com>
2023-08-28 11:15:34 +0800

    进行跨域处理

--- src/main/java/com/jk/my_blog/shiro/JwtFilter.java
+++ src/main/java/com/jk/my_blog/shiro/JwtFilter.java
@@ -76,4 +76,21 @@ public class JwtFilter extends AuthenticatingFilter {
         }
         return false;
     }
+
+    @Override
+    protected boolean preHandle(ServletRequest request, ServletResponse response) throws Exception {
+
+        HttpServletRequest httpServletRequest = WebUtils.toHttp(request);
+        HttpServletResponse httpServletResponse = WebUtils.toHttp(response);
+        httpServletResponse.setHeader("Access-control-Allow-Origin", httpServletRequest.getHeader("Origin"));
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS,PUT,DELETE");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers", httpServletRequest.getHeader("Access-Control-Request-Headers"));
+        // 跨域时会首先发送一个OPTIONS请求，这里我们给OPTIONS请求直接返回正常状态
+        if (httpServletRequest.getMethod().equals(RequestMethod.OPTIONS.name())) {
+            httpServletResponse.setStatus(org.springframework.http.HttpStatus.OK.value());
+            return false;
+        }
+
+        return super.preHandle(request, response);
+    }
 }
