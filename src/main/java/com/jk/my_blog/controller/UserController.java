5bc21e7f32e7b28ef5867dec034e6d2e50eb06e1
“Kawhi-95” <420021407@qq.com>
2023-08-28 11:14:57 +0800

    进行实体校验

--- src/main/java/com/jk/my_blog/controller/UserController.java
+++ src/main/java/com/jk/my_blog/controller/UserController.java
@@ -2,13 +2,12 @@ package com.jk.my_blog.controller;
 
 
 import com.jk.my_blog.common.lang.Result;
+import com.jk.my_blog.entity.User;
 import com.jk.my_blog.service.UserService;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -31,5 +30,9 @@ public class UserController {
         return Result.success(userService.getById(1L));
     }
 
+    @PostMapping("/save")
+    public Result save(@Validated @RequestBody User user){
+        return Result.success(user);
+    }
 
 }
